4e8e3d84d5527b78ab3bf3cb7c67fcee9129a4a1
jealmeida021 <136184014+jealmeida021@users.noreply.github.com>
2026-07-08 10:31:44 -0300

    Update Endereco.java

--- src/Endereco.java
+++ src/Endereco.java
@@ -1,2 +1,24 @@
-package PACKAGE_NAME;public class Endereco {
+public class Endereco  {
+
+    String tipoLogradouro;
+    String numero;
+    String logradouro;
+    String bairro;
+    String cidade;
+    String cep;
+    String estado;
+
+
+    @Override
+    public String toString() {
+        return "Endereco{" +
+                "tipoLogradouro='" + tipoLogradouro + '\'' +
+                ", numero='" + numero + '\'' +
+                ", logradouro='" + logradouro + '\'' +
+                ", bairro='" + bairro + '\'' +
+                ", cidade='" + cidade + '\'' +
+                ", cep='" + cep + '\'' +
+                ", estado='" + estado + '\'' +
+                '}';
+    }
 }
