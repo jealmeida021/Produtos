df15abe0b1fd2f07473e3d3d55c5d40fce58b6c8
jealmeida021 <136184014+jealmeida021@users.noreply.github.com>
2026-07-23 10:22:29 -0300

    Update Produto.java

--- src/Produto.java
+++ src/Produto.java
@@ -1,9 +1,45 @@
 public class Produto {
+    private int id;
+    private String nome;
+    private String categoria;
+    private double valor;
 
- int id ;
- String nome;
- String categoria;
- double valor;
+    public Produto() {
+    }
 
+    public int getId() {
+        return this.id;
+    }
 
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCategoria() {
+        return this.categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public double getValor() {
+        return this.valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    public String toString() {
+        return "Produto{id=" + this.id + ", nome='" + this.nome + "', categoria='" + this.categoria + "', valor=" + this.valor + "}";
+    }
 }
