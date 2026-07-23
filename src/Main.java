5d4be2c3b56de175ef02851b61e4f0aead9ec8ba
jealmeida021 <136184014+jealmeida021@users.noreply.github.com>
2026-07-23 10:20:34 -0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,47 +1,26 @@
 public class Main {
-
-
-    public static void main(String[] args) {
-
-        Main main = new Main();
-        main.realizaCadastro();
-
+    public Main() {
     }
 
-        public void realizaCadastro(){
+    public static void main(String[] args) {
+        Main main = new Main();
+        main.realizaCadastro();
+    }
 
-
-            Consumidor consumidor = new Consumidor();
-            Consumidor consumidor1 = new Consumidor();
-            consumidor.nome = "Aline";
-            consumidor.cpf = "123456789";
-            consumidor.endereco = new Endereco();
-            consumidor.endereco.cep = "15000";
-            consumidor.endereco.numero = "150";
-            consumidor.endereco.estado = "Sao Paulo";
-            consumidor.endereco.cidade = "Sorocaba";
-            consumidor.endereco.bairro = "Cajuru";
-            consumidor.endereco.logradouro = "Praca";
-            consumidor.endereco.tipoLogradouro = "Ponto";
-
-            consumidor1.nome = "Isabella";
-            consumidor1.cpf = "1212121212";
-            consumidor1.endereco = new Endereco();
-            consumidor1.endereco.cep = "1810500";
-            consumidor1.endereco.numero = "120";
-            consumidor1.endereco.estado = "Sao Paulo";
-            consumidor1.endereco.cidade = "Sorocaba";
-            consumidor1.endereco.bairro = "Cajuru";
-            consumidor1.endereco.logradouro = "Rua Jose";
-            consumidor1.endereco.tipoLogradouro = "Praça";
-
-
-
-            System.out.println(consumidor.endereco);
-
-            System.out.println(consumidor1.endereco);
-
-        }
-
-
+    public void realizaCadastro() {
+        Consumidor consumidor = new Consumidor();
+        consumidor.setNome("Aline");
+        consumidor.setCpf("123456");
+        consumidor.getEndereco().setLogradouro("Rua jose");
+        consumidor.getEndereco().setCep("1815151");
+        consumidor.getEndereco().setEstado("SP");
+        consumidor.getEndereco().setCidade("Sorocaba");
+        consumidor.getEndereco().setBairro("Cajuru");
+        consumidor.getEndereco().setTipoLogradouro("Residencial");
+        consumidor.getEndereco().setNumero("123");
+        System.out.println("Nome : " + consumidor.getNome());
+        System.out.println("CPF : " + consumidor.getCpf());
+        System.out.println("Endereco : " + String.valueOf(consumidor.getEndereco()));
+    }
 }
+
